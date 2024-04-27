41874b78ff171a709802f4219302bf39a0b200a3
aaashu202 <167896518+aaashu202@users.noreply.github.com>
2024-04-27 15:15:36 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -76,17 +76,16 @@ async def start_command(client: Client, message: Message):
                 reply_markup = None
 
             try:
-             lop = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                await asyncio.sleep(e.x)
+              lop = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                await asyncio.sleep(0.5)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
             except:
                 pass
         return
-             await asyncio.sleep(30)
-             await lop.delete()
-        
+        await asyncio.sleep(30)
+        await lop.delete() 
         
     else:
         reply_markup = InlineKeyboardMarkup(
